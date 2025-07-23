86e7b8fd9250d8bb2d27906ee4ec42d11788bd35
Wilian Morales <willian.moralesch@gmail.com>
2025-07-22 22:51:50 -0500

    Agregar DTOs para Category y CreateCategory con validaciones

---
+++ Models/Dtos/CreateCategoryDto.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ecommerceApi_netcore_devtalles.Models.Dtos;
+
+public class CreateCategoryDto
+{
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [MaxLength(50, ErrorMessage = "El nombre no puede exceder los 50 caracteres.")]
+    [MinLength(3, ErrorMessage = "El nombre debe tener al menos 3 caracteres.")]
+    public string Name { get; set; } = string.Empty;
+}
