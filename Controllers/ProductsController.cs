1dd275c5d6e94a048d7574ae3abc8279524b8380
Wilian Morales <willian.moralesch@gmail.com>
2025-07-23 16:40:32 -0500

    Implementar ProductsController con inyección de dependencias de IProductRepository y AutoMapper, y creacion de método GetProducts

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+using AutoMapper;
+using ecommerceApi_netcore_devtalles.Models.Dtos;
+using ecommerceApi_netcore_devtalles.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ecommerceApi_netcore_devtalles.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetProducts()
+        {
+            var products = _productRepository.GetAllProducts();
+            if (products == null || !products.Any())
+            {
+                return NotFound("No se encontraron productos.");
+            }
+            
+            var productsDto = _mapper.Map<List<ProductDto>>(products);
+            return Ok(productsDto);
+        }
+    }
+}
