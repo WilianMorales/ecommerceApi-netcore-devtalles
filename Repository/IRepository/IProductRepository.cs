b9dde48e38cdab2fb78f6431d753c151bd54ec98
Wilian Morales <willian.moralesch@gmail.com>
2025-07-23 16:21:49 -0500

    Agregado IProductRepository con operaciones CRUD e implementación de ProductRepository con funcionalidad de búsqueda

---
+++ Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using ecommerceApi_netcore_devtalles.Models;
+
+namespace ecommerceApi_netcore_devtalles.Repository.IRepository;
+
+public interface IProductRepository
+{
+    ICollection<Product> GetAllProducts();
+    ICollection<Product> GetProductsByCategory(int categoryId);
+    ICollection<Product> SearchProduct(string name);
+    Product? GetProductById(int id);
+    bool BuyProduct(string name, int quantity);
+    bool ProductExists(int id);
+    bool ProductExists(string name);
+    bool CreateProduct(Product product);
+    bool UpdateProduct(Product product);
+    bool DeleteProduct(Product product);
+    bool Save();
+}
