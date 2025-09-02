5632dbca114654921a68194d4bfefa11349ba2bd
Wilian Morales <willian.moralesch@gmail.com>
2025-09-02 00:30:30 -0500

    Implementación de UsersController y registre IUserRepository en program.cs
    - Se implemento los endpoint GetUsers, GetUserById y RegisterUser en el controlador.
    - Se registro  IUserRepository en el contenedor de inyección de dependencias de program.cs

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,93 @@
+using AutoMapper;
+using ecommerceApi_netcore_devtalles.Models.Dtos;
+using ecommerceApi_netcore_devtalles.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ecommerceApi_netcore_devtalles.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UsersController(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetUsers()
+        {
+            var users = _userRepository.GetUsers();
+            if (users == null || !users.Any())
+            {
+                return NotFound("No se encontraron usuarios.");
+            }
+            var usersDto = _mapper.Map<List<UserDto>>(users);
+            return Ok(usersDto);
+        }
+
+        [HttpGet("{id:int}", Name = "GetUserById")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetUserById(int id)
+        {
+            var user = _userRepository.GetUserById(id);
+            if (user == null || user.Id == 0)
+            {
+                return NotFound($"No se encontró al Usuario con ID {id}.");
+            }
+
+            var userDto = _mapper.Map<UserDto>(user);
+            if (userDto == null)
+            {
+                return NotFound($"No se pudo mapear al usuario con ID {id}.");
+            }
+            return Ok(userDto);
+        }
+
+        [HttpPost(Name = "RegisterUser")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> RegisterUser([FromBody] CreateUserDto createUserDto)
+        {
+            if (createUserDto == null || !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (string.IsNullOrWhiteSpace(createUserDto.Username))
+            {
+                ModelState.AddModelError("", "Username es requerido.");
+                return BadRequest(ModelState);
+            }
+
+            if (!_userRepository.IsUniqueUser(createUserDto.Username))
+            {
+                ModelState.AddModelError("", "El usuario ya existe.");
+                return BadRequest(ModelState);
+            }
+
+            var result = await _userRepository.Register(createUserDto);
+            if (result == null)
+            {
+                ModelState.AddModelError("", "Ocurrió un error al registrar al usuario.");
+                return StatusCode(StatusCodes.Status500InternalServerError, ModelState);
+            }
+
+            return CreatedAtRoute("GetUserById", new { id = result.Id }, result);
+        }
+
+    }
+}
